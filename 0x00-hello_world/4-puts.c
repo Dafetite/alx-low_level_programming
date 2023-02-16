b225f8428fdb9b1cfa9f74b7859132d2ef1ccc7f
Dafetite <ogagadafetite@gmail.com>
2023-02-16 07:43:20 -0800

    auto-alias-push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
-*This code takes no stdin argument but prints the str(Mystr) to stdout
-*main - takes no stdin
-*/
+ *This code prints out str(Mystr)
+ */
 int main(void)
 {
 char Mystr[] = "\"Programming is like building a multilingual puzzle";
