1c234c35c236ab98016a9d83c4041c1a994b9a52
Dafetite <ogagadafetite@gmail.com>
2023-02-17 07:47:32 -0800

    auto-alias-push

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,9 @@
 int main(void)
 {
 
-char alpha[] = "abcdfghijklmnoprstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char alpha[] = "abcdfghijklmnoprstuvwxyz";
 int dafe;
-for (dafe = 0; dafe < 50; dafe++)
+for (dafe = 0; dafe < 24; dafe++)
 putchar(alpha[dafe]);
 putchar('\n');
 return (0);
