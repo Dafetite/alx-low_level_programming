5c0bd070f8311efa1a92abbd038435dde8c1b3b8
Dafetite <ogagadafetite@gmail.com>
2023-02-17 07:33:55 -0800

    auto-alias-push

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+  char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$";
+  int dafe;
+  for (dafe = 0; dafe < 26; dafe++)
+    putchar(alpha[dafe]);
+  putchar('\n');
+  return (0);
+}
