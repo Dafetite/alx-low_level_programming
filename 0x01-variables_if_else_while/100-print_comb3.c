c3f2745478808dbdcd25382e3fd7b99566b725a3
Dafetite <ogagadafetite@gmail.com>
2023-02-17 11:00:27 -0800

    another auto-alias-push

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,11 +11,11 @@ for (i = 48; i <= 56; i++)
 {
 for (j = 48; j <= 57; j++)
 {
-if (j > i )
+if (j > i)
 {
 putchar(i);
 putchar(j);
-if (i != 56 || j != 57 )
+if (i != 56 || j != 57)
 {
 putchar(',');
 putchar(' ');
