8f4fdb4bb43ea6a7fd4c15719159aca6267ab679
Dafetite <ogagadafetite@gmail.com>
2023-02-17 07:51:11 -0800

    auto-alias-push

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char alpha[] = "0123456789";
+int dafe;
+for (dafe = 0; dafe < 10; dafe++)
+putchar(alpha[dafe]);
+putchar('\n');
+return (0);
+}
