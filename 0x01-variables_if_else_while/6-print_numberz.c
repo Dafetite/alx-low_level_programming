1f23eb5f2a678316e9de2e16f1d8f3dd62de30f6
Dafetite <ogagadafetite@gmail.com>
2023-02-17 07:55:18 -0800

    auto-alias-push

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int dafe;
+for (dafe = 0; dafe < 10; dafe++)
+putchar(dafe);
+putchar('\n');
+return (0);
+}
