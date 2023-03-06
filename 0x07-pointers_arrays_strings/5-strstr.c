79d310e94103c9a45ee78275df08f6a5bbc57d79
Dafetite <ogagadafetite@gmail.com>
2023-03-06 19:44:09 +0100

    5.1

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,7 +6,8 @@
  * Return: Always 0 (Success)
  */
 char *_strstr(char *haystack, char *needle)
-{	for (; *haystack != '\0'; haystack++)
+{
+	for (; *haystack != '\0'; haystack++)
 	{
 		char *l = haystack;
 		char *p = needle;
@@ -16,9 +17,9 @@ char *_strstr(char *haystack, char *needle)
 			l++;
 			p++;
 		}
-
- 		if (*p == '\0')
+		if (*p == '\0')
 			return (haystack);
-	} 	return (0);
+	}
+ 	return (0);
 }
 
