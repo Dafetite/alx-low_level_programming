752fbe1e6f8a94bb742dadc0ee1c6bf80b2886f3
Dafetite <ogagadafetite@gmail.com>
2023-03-06 22:48:20 +0100

    7.3

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 /**
  * print_diagsums - Entry point
- * @a: input * @size: input
+ * @a: input
+ * @size: input
  * Return: Always 0 (Success)
  */
 void print_diagsums(int *a, int size)
