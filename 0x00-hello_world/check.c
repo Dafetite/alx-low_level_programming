c4df4cd1f292ad565f59978019268f844f8cc27b
Dafetite <ogagadafetite@gmail.com>
2023-03-11 19:35:06 +0100

    commiting and pushing right from vscode through my subsystem

---
+++ 0x00-hello_world/check.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char Mystr[] = "HELLO THERE!";
+printf("just testing right from my vscode through my subsystem, crazy, right?\n%s\n", Mystr);
+return (0);
+}
